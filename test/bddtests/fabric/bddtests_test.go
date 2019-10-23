f64d5d0f96b0923ad09e527719fd74e3d9aa22f5
Troy Ronda <troy.ronda@securekey.com>
2019-10-22 20:52:10 -0400

    ci: azure pipelines
    Signed-off-by: Troy Ronda <troy.ronda@securekey.com>

--- test/bddtests/fabric/bddtests_test.go
+++ test/bddtests/fabric/bddtests_test.go
@@ -10,6 +10,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"testing"
@@ -24,6 +25,13 @@ import (
 var composition *bddtests.Composition
 
 func TestMain(m *testing.M) {
+	projectPath, err := filepath.Abs("../../..")
+ 	if err != nil {
+ 		panic(err.Error())
+ 	}
+ 	if err := os.Setenv("PROJECT_PATH", projectPath); err != nil {
+ 		panic(err.Error())
+ 	}
 
 	// default is to run all tests with tag @all
 	tags := "all"
@@ -51,7 +59,7 @@ func TestMain(m *testing.M) {
 				composition = newComposition
 
 				fmt.Println("docker-compose up ... waiting for peer to start ...")
-				testSleep := 5
+				testSleep := 20
 				if os.Getenv("TEST_SLEEP") != "" {
 					testSleep, _ = strconv.Atoi(os.Getenv("TEST_SLEEP"))
 				}
